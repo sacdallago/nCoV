d7f57f402b924105535ab88227d548f719619845
Christian Dallago <code@dallago.us>
2020-02-05 14:36:29 +0100

    new scripts

--- get_sequence_info.py
+++ get_sequence_info.py
@@ -1,6 +1,8 @@
 #!/bin/python
 
 import argparse
+import xml.etree.ElementTree as ET
+from pandas import DataFrame
 from Bio import Entrez
 
 parser = argparse.ArgumentParser(description='Searches for protein sequences in the Title Word field ([TITL]) based on any provided key terms.\nSee here for further details: http://cbsu.tc.cornell.edu/resources/seq_comp/pb607_introductory/entrez/ncbi_entrez.html')
@@ -16,7 +18,27 @@ handle = Entrez.esearch(db='protein', term=arguments.SearchTerm, retmax=100000)
 results = Entrez.read(handle)
 handle.close()
 
-with open((arguments.FileName if arguments.FileName else 'sequences') + '.fasta', "w") as fasta_file:
-    handle = Entrez.efetch(db='protein', id=results['IdList'], rettype='fasta')
-    print(handle.read(), file=fasta_file)
-    handle.close()
+handle = Entrez.efetch(db='protein', id=results['IdList'], rettype='gp', retmode='xml')
+tree = ET.fromstring(handle.read())
+handle.close()
+
+proteins = []
+
+for child in tree:
+    protein = {
+        'locus': child.find('GBSeq_locus').text,
+        'primary_accession': child.find('GBSeq_primary-accession').text,
+        'accession_version': child.find('GBSeq_accession-version').text,
+    }
+
+    for qualifier in child.findall('.//GBQualifier'):
+        if qualifier.find('GBQualifier_name').text == 'isolate':
+            protein['isolate'] = qualifier.find('GBQualifier_value').text
+
+    proteins.append(protein)
+
+df = DataFrame(proteins)
+df.to_csv((arguments.FileName if arguments.FileName else 'sequences') + '.txt',
+          sep='\t',
+          index=False)
+
